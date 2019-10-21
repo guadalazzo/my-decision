d9132967338759d5dad71cbf58ce561bdb07e7fe
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2019-10-21 00:01:43 -0300

    added a home with arrows

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section className="arrows">
+            <span className="arrow-1"></span>
+            <span className="arrow-2"></span>
+            <span className="arrow-3"></span>
+        </section>
+        <button>New Decision</button> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
