3d48e709a5afac8f02c91001d0757c1a21212a14
guadalupe.lazzo <guadalupe.lazzo@comeon.com>
2023-02-11 13:59:00 +0100

    updating

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const cors = require('cors')
-const app = require('express')();
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const app = require("express")();
+
 app.use(cors());
 var firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -10,7 +11,7 @@ var firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.API_ID
+  appId: process.env.API_ID,
 };
 
 admin.initializeApp(firebaseConfig);
@@ -19,45 +20,51 @@ admin.initializeApp(firebaseConfig);
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 /* Get all dilemmas */
-app.get('/dilemmas', (req,res) => {
-  admin.firestore().collection('dilemmas').get()
-  .then(data => {
+app.get("/dilemmas", (req, res) => {
+  admin
+    .firestore()
+    .collection("dilemmas")
+    .get()
+    .then((data) => {
       let dilemmas = [];
-      data.forEach(doc => {
-          dilemmas.push(doc.data());
-      })
+      data.forEach((doc) => {
+        dilemmas.push(doc.data());
+      });
       return res.json(dilemmas);
-  })
-  .catch(err=>{console.log('Error with getting dillemmas', err)});
+    })
+    .catch((err) => {
+      console.log("Error with getting dillemmas", err);
+    });
 });
 
 /* Get a dilemma */
 
-app.get('/dilemma/:id', (req,res) => {
+app.get("/dilemma/:id", (req, res) => {
   let dilemmaData = {};
-  admin.firestore()
+  admin
+    .firestore()
     .collection("dilemmas")
     .where("id", "==", req.params.id)
     .limit(1)
     .get()
     .then((doc) => {
       if (!doc.docs[0].exists) {
-        return res.status(404).json({ error: 'Dilemma not found' });
+        return res.status(404).json({ error: "Dilemma not found" });
       }
       dilemmaData = doc.docs[0].data();
       return res.json(dilemmaData);
     })
     .catch((err) => {
-      console.error(`Error getting dilemma`,err);
+      console.error(`Error getting dilemma`, err);
       res.status(500).json({ error: err.code });
     });
 });
 
 /* Delete a dilemma */
 
-app.delete('/dilemma/:id', (req,res) => {
-
-  const document = admin.firestore()
+app.delete("/dilemma/:id", (req, res) => {
+  const document = admin
+    .firestore()
     .collection("dilemmas")
     .where("id", "==", req.params.id)
     .limit(1);
@@ -65,24 +72,24 @@ app.delete('/dilemma/:id', (req,res) => {
     .get()
     .then((doc) => {
       if (!doc.docs[0].exists) {
-        return res.status(404).json({ error: 'Dilemma not found' });
+        return res.status(404).json({ error: "Dilemma not found" });
       }
       return admin.firestore().doc(`/dilemmas/${doc.docs[0].id}`).delete();
     })
-  .then(() => 
-    res.json({ message: 'Dilemma deleted successfully' })
-  )
-  .catch((err) => {
-    console.error(err);
-    return res.status(500).json({ error: err.code, message:'this is a error' });
-  });
+    .then(() => res.json({ message: "Dilemma deleted successfully" }))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: err.code, message: "this is a error" });
+    });
 });
 
 /* Update a dilemma */
 
-app.put('/dilemma/:id', (req,res) => {
+app.put("/dilemma/:id", (req, res) => {
   const dilemmaData = {
-    title: req.body.title, 
+    title: req.body.title,
     conArgs: req.body.conArgs,
     proArgs: req.body.proArgs,
     totalPro: req.body.totalPro,
@@ -90,7 +97,8 @@ app.put('/dilemma/:id', (req,res) => {
     totalPoints: req.body.totalPoints,
     id: req.params.id,
   };
-  const document = admin.firestore()
+  const document = admin
+    .firestore()
     .collection("dilemmas")
     .where("id", "==", req.params.id)
     .limit(1);
@@ -98,42 +106,50 @@ app.put('/dilemma/:id', (req,res) => {
     .get()
     .then((doc) => {
       if (!doc.docs[0].exists) {
-        return res.status(404).json({ error: 'Dilemma not found' });
+        return res.status(404).json({ error: "Dilemma not found" });
       }
 
-      return admin.firestore().collection("dilemmas").doc(doc.docs[0].id).update({...dilemmaData});
+      return admin
+        .firestore()
+        .collection("dilemmas")
+        .doc(doc.docs[0].id)
+        .update({ ...dilemmaData });
     })
-  .then(() => 
-    res.json({ message: `Dilemma updated successfully` })
-  )
-  .catch((err) => {
-    console.error(err);
-    return res.status(500).json({ error: err.code, message:'this is a error' });
-  });
+    .then(() => res.json({ message: `Dilemma updated successfully` }))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: err.code, message: "this is a error" });
+    });
 });
 
 /* Create a dilemma */
 
-app.post('/dilemma', (req,res) => { //  crea dilemmas en el array de dilemmas
-  const newDilemma = { 
+app.post("/dilemma", (req, res) => {
+  //  crea dilemmas en el array de dilemmas
+  const newDilemma = {
     id: Math.random().toString(26).slice(2),
-    title: req.body.title, 
+    title: req.body.title,
     conArgs: req.body.conArgs,
     proArgs: req.body.proArgs,
     totalPro: req.body.totalPro,
     totalCons: req.body.totalCons,
     totalPoints: req.body.totalPoints,
   };
-  admin.firestore()
-  .collection('dilemmas')
+  admin
+    .firestore()
+    .collection("dilemmas")
     .add(newDilemma)
     .then((doc) => {
-      res.json({message: `Document ${doc.id} was created succesfully`});
+      res.json({ message: `Document ${doc.id} was created succesfully` });
       return doc.data();
     })
     .catch((err) => {
-      console.error('Error creating dilemma',err);
-      res.status(500).json({ message: 'Something went wrong', error: err.code });
+      console.error("Error creating dilemma", err);
+      res
+        .status(500)
+        .json({ message: "Something went wrong", error: err.code });
     });
 });
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,16 @@
-import React,{ useEffect, useState }from 'react';
-import './styles.scss';
-import axios from 'axios';
-import {Loading} from './../../components/Loading';
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+import axios from "axios";
+import { Loading } from "./../../components/Loading";
 function App(props) {
-
-  const [ hasDilemmas,setHasDilemma ] =  useState(false);
-  const [ isLoading, setLoading ] = useState(true);
+  const [hasDilemmas, setHasDilemma] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
     axios
       .get(
         "https://us-central1-my-decision-ad541.cloudfunctions.net/api/dilemmas"
       )
-      .then(response => {
+      .then((response) => {
         setHasDilemma(response.data.length !== 0);
         setLoading(false);
         return response.data;
@@ -19,20 +18,24 @@ function App(props) {
   }, []);
 
   const handleClick = () => {
-    return !hasDilemmas ? props.history.push("/new-dilemma") : props.history.push("/dilemmas");
-  }
+    return !hasDilemmas
+      ? props.history.push("/new-dilemma")
+      : props.history.push("/dilemmas");
+  };
   return (
     <div className="App">
-      {isLoading ?  
-      <Loading /> :
-      <header className="App-header">
-        <section className="arrows">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <header className="App-header">
+          <section className="arrows">
             <span className="arrow-1"></span>
             <span className="arrow-2"></span>
             <span className="arrow-3"></span>
-        </section>
-        <button onClick={handleClick}>New Decision</button> 
-      </header>}
+          </section>
+          <button onClick={handleClick}>New Decision</button>
+        </header>
+      )}
     </div>
   );
 }
